1e9cc81ae3cf7256cec9378e3158539295af4a36
buihuutrii <21130575@st.hcmuaf.edu.vn>
2026-06-19 19:29:25 +0700

    tao interface IKeyLossReportDao

--- src/main/java/vn/edu/hcmuaf/fit/dao/IKeyLossReportDao.java
+++ src/main/java/vn/edu/hcmuaf/fit/dao/IKeyLossReportDao.java
@@ -1,1 +1,22 @@
+package vn.edu.hcmuaf.fit.dao;
 
+import vn.edu.hcmuaf.fit.model.KeyLossReportModel;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface IKeyLossReportDao {
+
+
+    boolean submitReport(int idKey, int idUser, String reason);
+
+    List<KeyLossReportModel> getPendingReports();
+
+    List<KeyLossReportModel> getAllReports();
+
+    boolean approveReport(int idReport, String adminNote);
+
+    boolean rejectReport(int idReport, String adminNote);
+
+    boolean hasPendingReport(int idKey);
+}
